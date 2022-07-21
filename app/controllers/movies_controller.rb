3c17be15b96ae7799f32201dae46c5309dff32dc
Tuncay Aydin <35038225+hal-9@users.noreply.github.com>
2022-07-21 17:43:47 +0200

    create view and get movie-data into there

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,23 @@
 class MoviesController < ApplicationController
+  before_action :find_movies
+
   def index
-    @movies = Movie.all
+  end
+
+  def find_movies
+    if params[:filter] == 'by_genre'
+      @movies = Movie.all.by_genre(params[:parameter])
+    elsif params[:filter] == 'by_decade'
+      start_date = params[:parameter][0..3]
+      end_date = params[:parameter][5..8]
+      @movies = Movie.all.by_decade(start_date, end_date)
+    elsif params[:filter] == 'favorite_movies'
+      @movies = Movie.all.favorite_movies
+    else
+      @movies = Movie.all
+    end
+  end
+
+  def show
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  scope :favorite_movies, -> {where(rating: 5)}
+  scope :least_favorite_movies, -> {where(rating: 1)}
+  scope :by_decade, -> (start_date, end_date) { where(year: start_date..end_date) }
+  scope :by_genre, -> (genre) { where("genre like?", "%#{genre}%")}
 end
